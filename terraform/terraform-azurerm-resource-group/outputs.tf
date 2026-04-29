8f97cfe49567cc6593e4717e0ed906d4055834b9
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2026-04-29 23:25:28 +0530

    feat: add Terraform configuration for Azure Resource Group and outputs (#70)

---
+++ terraform/terraform-azurerm-resource-group/outputs.tf
@@ -0,0 +1,16 @@
+output "rg_location" {
+    description = "Location of the resource group"
+    value       = azurerm_resource_group.rg.location
+}
+
+output "rg_name" {
+    description = "Name of the resource group"
+    value       = azurerm_resource_group.rg.name
+  
+}
+
+output "rg_id" {
+    description = "ID of the resource group"
+    value       = azurerm_resource_group.rg.id
+  
+}
