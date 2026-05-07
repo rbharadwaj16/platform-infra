5a0865f210e52e47d33553da89566094b67a6e56
rbharadwaj16 <rbharadwaj16@gmail.com>
2026-05-07 22:39:42 +0530

    feat: refactor resource group variables for clarity and consistency

--- terraform/terraform-azurerm-resource-group/variables.tf
+++ terraform/terraform-azurerm-resource-group/variables.tf
@@ -1,20 +1,43 @@
-variable "resource_group_name" {
-  description = "Name of the resource group to be created"
+variable "name" {
+  description = "Explicit resource group name. If provided, this overrides context-based naming."
   type        = string
-  
-}
-
-variable "location" {
-  description = "Azure region where the resource group will be created"
-  type        = string
+  default     = null
 }
 
 variable "context" {
-  
+  description = "Naming context used to compute the resource group name when name is not provided."
+
+  type = object({
+    org      = optional(string)
+    domain   = optional(string)
+    app      = string
+    env      = string
+    region   = string
+    instance = optional(string)
+  })
+
+  default = null
+}
+
+variable "location" {
+  description = "Azure region where the resource group will be created."
+  type        = string
 }
 
 variable "tags" {
-  description = "Tags to be applied to the resource group"
+  description = "Tags to apply to the resource group."
   type        = map(string)
   default     = {}
+}
+
+variable "required_tag_keys" {
+  description = "Required tag keys that must exist and contain non-empty values."
+  type        = set(string)
+  default     = ["env", "owner", "cost_center"]
+}
+
+variable "allowed_locations" {
+  description = "Allowed Azure regions. Empty set means no restriction."
+  type        = set(string)
+  default     = []
 }
