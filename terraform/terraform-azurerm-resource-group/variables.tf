4e97edd2c4e533254bfe4e097be0f3b0f09d6891
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2026-05-14 23:34:37 +0530

    feat: simplify variables.tf by removing unused variables and streamlining definitions (#76)

--- terraform/terraform-azurerm-resource-group/variables.tf
+++ terraform/terraform-azurerm-resource-group/variables.tf
@@ -1,43 +1,11 @@
 variable "name" {
   description = "Explicit resource group name. If provided, this overrides context-based naming."
-  type        = string
-  default     = null
-}
-
-variable "context" {
-  description = "Naming context used to compute the resource group name when name is not provided."
-
-  type = object({
-    org      = optional(string)
-    domain   = optional(string)
-    app      = string
-    env      = string
-    region   = string
-    instance = optional(string)
-  })
-
+  type = string
   default = null
 }
 
 variable "location" {
   description = "Azure region where the resource group will be created."
-  type        = string
+  type = string
 }
 
-variable "tags" {
-  description = "Tags to apply to the resource group."
-  type        = map(string)
-  default     = {}
-}
-
-variable "required_tag_keys" {
-  description = "Required tag keys that must exist and contain non-empty values."
-  type        = set(string)
-  default     = ["env", "owner", "cost_center"]
-}
-
-variable "allowed_locations" {
-  description = "Allowed Azure regions. Empty set means no restriction."
-  type        = set(string)
-  default     = []
-}
