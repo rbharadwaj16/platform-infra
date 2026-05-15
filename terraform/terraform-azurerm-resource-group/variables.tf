3e51e8557a4e6838f004079a5083918eba8f5b36
Raghavendra Bharadwaj <rbharadwaj16@gmail.com>
2026-05-15 22:33:53 +0530

    feat: add additional variables for resource group configuration and tagging (#77)

--- terraform/terraform-azurerm-resource-group/variables.tf
+++ terraform/terraform-azurerm-resource-group/variables.tf
@@ -9,3 +9,39 @@ variable "location" {
   type = string
 }
 
+variable "name" {
+  description = "Explicit rg name. If provided, this overrides context-based naming"
+}
+
+variable "context" {
+  description = "Naming context used to compute the resource group name when name is not provided"
+  
+  type = object({
+    org      = optional(string)
+    domain   = optional(string)
+    app      = string
+    env      = string
+    region   = string
+    instance  = optional(string)
+  })
+
+  default = null
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource group."
+  type = map(string)
+  default = {} 
+}
+
+variable "required_tag_keys" {
+  description = "Required tag keys that must exist and contain non-empty values."
+  type        = set(string)
+  default     = ["env", "owner", "cost_center"]
+}
+
+variable "allowed_locations" {
+  description = "Allowed Azure regions. Empty set means no restriction."
+  type        = set(string)
+  default     = []
+}
